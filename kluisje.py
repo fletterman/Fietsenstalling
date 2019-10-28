31bb8bf52df0b67a32321e9abf62c00df0350e90
spark-156 <57094525+spark-156@users.noreply.github.com>
2019-10-28 12:56:56 +0100

    asdasd

--- kluisje.py
+++ kluisje.py
@@ -1,1 +1,13 @@
 #basis
+import json
+from tkinter import *
+
+while True:
+    kaartNummer = input('Wat is uw kaartnummer?')
+    with open("fietsenstallingen.json") as infile:
+        kluisjes = infile.read()
+    for x in kluisjes:
+        if kaartNummer in x:
+            print('ja')
+        else:
+            print('nee')
