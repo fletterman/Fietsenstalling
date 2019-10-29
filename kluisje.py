5a551f22118f7a2c066190c4bc7212a95884a600
spark-156 <57094525+spark-156@users.noreply.github.com>
2019-10-29 11:15:22 +0100

    Functieverbetering

--- kluisje.py
+++ kluisje.py
@@ -14,22 +14,27 @@ def nieuweKluis(kaartNummer):
     stallingsJaar = vandaag.strftime('%Y')
     stallingsMaand
 
-def totaalMinuten(stallingsJaar, stallingsMaand, stallingsDag, stallingsUur, stallingsMinuut):
-    pass
-
-def stallingsTijd(stallingsDatum):
+def totaalMinuten(datumDictionary):
     som = 0
     dagenInMaanden = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for x  in range(datumDictionary['stallingsMaand'] + 1): #ant dagen in huidige jaar berekenen door
+        som += dagenInMaanden[x]
+    antMinuten = (((som + int(huidigeJaar) * 365 + int(huidigeJaar) // 4) * 24 + int(huidigeDag)) * 60 + int(huidigeMinuut))  # ant jaar * 365 + 1 dag per vier hele jaren + ant dagen in huidige jaar
+    return antMinuten
+def huidigeDatum():
     maanden = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
     huidigeMaand, huidigeDag, huidigeJaar, huidigeUur, huidigeMinuut = vandaag.strftime('%b'), vandaag.strftime('%d'), vandaag.strftime('%Y'), vandaag.strftime("%H"), vandaag.strftime('%M')
     huidigeMaandNummer = maanden[huidigeMaand]
-    for x  in range(huidigeMaandNummer + 1): #ant dagen in huidige jaar berekenen door
-        som += dagenInMaanden[x]
-    #antHuidigeMinuten is optelling van aantal dagen in huidige aantal verstreken maanden + aantal jaar * 365 + 1 per vier gehele jaren + aantal huidige dagen.
-    #Dat keer 24 om naar uren te gaan, dat plus antHuidigeUren. Dat keer 60 plus huidigeMinuten voor het aantal huidige minuten die verstreken zijn sinds 0/0/0/0/0
-    antHuidigeMinuten = (((som + int(huidigeJaar) * 365 + int(huidigeJaar) // 4) * 24 + int(huidigeDag)) * 60 + int(huidigeMinuut)) #ant jaar * 365 + 1 dag per vier hele jaren + ant dagen in huidige jaar
-    print(antHuidigeMinuten)
-    print(huidigeJaar, huidigeMaandNummer, huidigeDag, huidigeUur, huidigeMinuut)
+    datumDictionary = {
+        "bezet": False,
+        "kaartNummer": 0,
+        "stallingsJaar": int(huidigeJaar),
+        "stallingsMaand": int(huidigeMaandNummer),
+        "stallingsDag": int(huidigeDag),
+        "stallingsUur": int(huidigeUur),
+        "stallingsMinuut": int(huidigeMinuut)
+    }
+    return datumDictionary
 
 while True:
     legeKluizen = 0
