99feade166581c9f52a44738047be7817451a1e9
spark-156 <57094525+spark-156@users.noreply.github.com>
2019-10-30 01:19:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 import requests
 import json
 token = "878137494:AAFq1YmAoh4bMGXeUBPM90hTJUMNdivlqw4"
 baseurl = "https://api.telegram.org/bot{}/".format(token)
 print(baseurl)
+=======
+token = "878137494:AAFq1YmAoh4bMGXeUBPM90hTJUMNdivlqw4"
+>>>>>>> Stashed changes
