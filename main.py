d8e128f3db54c69ebfeb336e718576634a2fc74b
spark-156 <57094525+spark-156@users.noreply.github.com>
2019-10-30 13:43:20 +0100

    Main2.py is compleet werkende versie van bot
    Werkt compleet, drie opties staan onderaan in while True: loop, deze moeten alleen geintegreerd worden in main code.

--- main.py
+++ main.py
@@ -7,23 +7,39 @@ def getUpdates(offset):
     url = baseurl + "getUpdates?timeout=120"
     if offset:
         url = url + "&offset={}".format(offset + 1)
-        print(offset, offset + 1, url)
+    print('started listening')
     result = requests.get(url)
+    print('message received', json.loads(result.content))
     return json.loads(result.content)
 
 def sendMessage(text, chat_id):
     url = baseurl + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     requests.get(url)
 
-update_id = None
-while True:
-    bericht = getUpdates(offset=update_id)
-    bericht = bericht["result"]
-    if bericht:
-        for item in bericht:
-            update_id = item["update_id"]
-            message = item["message"]["text"]
-            chat_id = item["message"]["chat"]["id"]
-            data = [update_id, message, chat_id]
-    if data[1] == '/start':
-        sendMessage('stuur "1" voor de huidigekosten die u heeft')
+def ovChipKaartNummerOphalen(chat_id):
+    sendMessage('Wat is uw ov-chipkaartnummer?', chat_id)
+
+def commands(update_id, message, chat_id):
+    if message == '/huidigeprijs':
+        ovChipKaartNummerOphalen(chat_id)
+        data = laatsteBerichtOphalen()
+        print(data)
+
+def laatsteBerichtOphalen(update_id):
+    data = []
+    while True:
+        bericht = getUpdates(offset=update_id)
+        bericht = bericht["result"]
+        if bericht:
+            print('message not empty')
+            for item in bericht:
+                update_id = item["update_id"]
+                message = item["message"]["text"]
+                chat_id = item["message"]["chat"]["id"]
+                data = [update_id, message, chat_id]
+                print(data)
+            return data #gevuld met alles
+        return data #lege lijst
+
+x = laatsteBerichtOphalen(None)
+commands(x[0], x[1], x[2])
---
+++ main2.py
@@ -0,0 +1,56 @@
+import requests
+import json
+token = "878137494:AAFq1YmAoh4bMGXeUBPM90hTJUMNdivlqw4"
+baseurl = "https://api.telegram.org/bot{}/".format(token)
+ovGegeven, update_id = False, None
+
+def getUpdates(offset=None):
+    url = baseurl + "getUpdates?timeout=120"
+    if offset:
+        url = url + "&offset={}".format(offset + 1)
+    result = requests.get(url)
+    return json.loads(result.content)
+
+def sendMessage(text, chat_id):
+    url = baseurl + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    requests.get(url)
+
+def ovChipKaartOphalen(chat_id, offset):
+    sendMessage('Wat is uw ov-chipkaartnummer?', chat_id)
+    while True:
+        ovchipkaartNummer = getUpdates(offset=offset)
+        global update_id
+        global ovGegeven
+        update_id += 1
+        ovGegeven = True
+        if ovchipkaartNummer['result']:
+            try:
+                ovchipkaartNummer = int(ovchipkaartNummer['result'][0]['message']['text'])
+            except:
+                sendMessage('U moet een getal opleveren bestaande alleen uit cijfers.', chat_id)
+                offset += 1
+                continue
+            return ovchipkaartNummer
+
+while True:
+    bericht = getUpdates(offset=update_id)
+    # print('hier')
+    bericht = bericht["result"]
+    if bericht:
+        for item in bericht:
+            update_id = item["update_id"]
+            message = item["message"]["text"]
+            chat_id = item["message"]["chat"]["id"]
+            if message == '/start':
+                ovNummer = ovChipKaartOphalen(chat_id, update_id)
+                sendMessage('Dankuwel, U kunt nu uit de volgende commands kiezen:\n1: "/huidigeprijs", hiermee ziet u uw huidige kosten. \n2: "/mijnkluisje", hiermee vindt u uw kluisnummer terug als u die bent vergeten. \n3: "/resterendetijd", hiermee berekent u de resterende tijd dat u uw fiets kan opslaan met uw huidige saldo.', chat_id)
+            elif message == '/huidigeprijs':
+                pass
+            elif message == '/mijnkluisje':
+                pass
+            elif message == '/resterendetijd':
+                pass
+            elif ovGegeven == False:
+                sendMessage('Met "/start" (zonder aanhalingstekens) kunt u beginnen.', chat_id)
+            else:
+                sendMessage('U kunt uit de volgende commands kiezen:\n1: "/huidigeprijs", hiermee ziet u uw huidige kosten. \n2: "/mijnkluisje", hiermee vindt u uw kluisnummer terug als u die bent vergeten. \n3: "/resterendetijd", hiermee berekent u de resterende tijd dat u uw fiets kan opslaan met uw huidige saldo.', chat_id)
